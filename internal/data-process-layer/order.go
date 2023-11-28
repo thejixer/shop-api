9f249fd3be0b137ecc6595859efeb6fb7e92a9c6
ali <thewhiterabbit1994@outlook.com>
2023-11-28 19:59:27 +0330

    minor cleanup

--- internal/data-process-layer/order.go
+++ internal/data-process-layer/order.go
@@ -1,7 +1,6 @@
 package dataprocesslayer
 
 import (
-	"github.com/shopspring/decimal"
 	"github.com/thejixer/shop-api/internal/models"
 )
 
@@ -22,30 +21,4 @@ func ConvertOrderItemtoDto(i *models.OrderItem) *models.OrderItemDto {
 		Quantity:       i.Quantity,
 		PriceAtTheTime: price,
 	}
-}
-
-func MakeOrder(o *models.Order, i []*models.OrderItem, u models.UserDto, a *models.AddressDto) *models.OrderDto {
-
-	var items []models.OrderItemDto
-
-	var temp decimal.Decimal
-
-	for _, e := range i {
-		items = append(items, *ConvertOrderItemtoDto(e))
-		Quantity := decimal.NewFromInt(int64(e.Quantity))
-		tp := Quantity.Mul(e.PriceAtTheTime)
-		temp = temp.Add(tp)
-	}
-	totalPrice, _ := temp.Float64()
-
-	return &models.OrderDto{
-		Id:         o.Id,
-		User:       u,
-		Address:    *a,
-		Status:     o.Status,
-		Items:      items,
-		CreatedAt:  o.CreatedAt,
-		TotalPrice: totalPrice,
-	}
-
 }
